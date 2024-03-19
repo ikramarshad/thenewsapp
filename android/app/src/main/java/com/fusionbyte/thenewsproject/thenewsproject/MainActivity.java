eeee4ac6eaea7065c9f14e07657a69a40567a9c2
cybertech009 <cybertech432@gmail.com>
2024-03-19 15:39:11 +0500

    Initial commit

---
+++ android/app/src/main/java/com/fusionbyte/thenewsproject/thenewsproject/MainActivity.java
@@ -0,0 +1,6 @@
+package com.fusionbyte.thenewsproject.thenewsproject;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:thenewsproject/view/home/home_screen.dart';
+
+import 'bloc/news_bloc.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<NewsBloc>(
+          create: (BuildContext context) => NewsBloc(),
+        ),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: const HomeScreen(),
+      ),
+    );
+  }
+}
+
+const spinkit = SpinKitChasingDots(
+  color: Colors.blue,
+  size: 40.0,
+);
